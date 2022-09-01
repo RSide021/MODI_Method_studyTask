656ab86c34f22c2ce32ecfccf626cb12e69c23e9
RSide021 <danila.sizih@gmail.com>
2022-09-01 22:07:49 +0800

    Uploading a project

---
+++ metodPotent/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace metodPotent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_solve = new System.Windows.Forms.Button();
+            this.textBox_info = new System.Windows.Forms.TextBox();
+            this.button_ok = new System.Windows.Forms.Button();
+            this.numericUpDownRow = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownClmn = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.button_color = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownClmn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_solve
+            // 
+            this.button_solve.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_solve.Location = new System.Drawing.Point(741, 18);
+            this.button_solve.Name = "button_solve";
+            this.button_solve.Size = new System.Drawing.Size(157, 50);
+            this.button_solve.TabIndex = 15;
+            this.button_solve.Text = "Рассчитать";
+            this.button_solve.UseVisualStyleBackColor = true;
+            this.button_solve.Click += new System.EventHandler(this.button_solve_Click);
+            // 
+            // textBox_info
+            // 
+            this.textBox_info.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox_info.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_info.Location = new System.Drawing.Point(12, 475);
+            this.textBox_info.Multiline = true;
+            this.textBox_info.Name = "textBox_info";
+            this.textBox_info.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_info.Size = new System.Drawing.Size(706, 338);
+            this.textBox_info.TabIndex = 14;
+            // 
+            // button_ok
+            // 
+            this.button_ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_ok.Location = new System.Drawing.Point(629, 18);
+            this.button_ok.Name = "button_ok";
+            this.button_ok.Size = new System.Drawing.Size(89, 50);
+            this.button_ok.TabIndex = 13;
+            this.button_ok.Text = "OK";
+            this.button_ok.UseVisualStyleBackColor = true;
+            this.button_ok.Click += new System.EventHandler(this.button_ok_Click);
+            // 
+            // numericUpDownRow
+            // 
+            this.numericUpDownRow.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownRow.Location = new System.Drawing.Point(501, 29);
+            this.numericUpDownRow.Name = "numericUpDownRow";
+            this.numericUpDownRow.Size = new System.Drawing.Size(73, 28);
+            this.numericUpDownRow.TabIndex = 12;
+            // 
+            // numericUpDownClmn
+            // 
+            this.numericUpDownClmn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownClmn.Location = new System.Drawing.Point(225, 29);
+            this.numericUpDownClmn.Name = "numericUpDownClmn";
+            this.numericUpDownClmn.Size = new System.Drawing.Size(73, 28);
+            this.numericUpDownClmn.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(334, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 24);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Строки (запасы):";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Столбцы (заказчики):";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 80);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(1364, 389);
+            this.dataGridView.TabIndex = 8;
+            // 
+            // button_color
+            // 
+            this.button_color.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_color.Location = new System.Drawing.Point(916, 18);
+            this.button_color.Name = "button_color";
+            this.button_color.Size = new System.Drawing.Size(116, 50);
+            this.button_color.TabIndex = 16;
+            this.button_color.Text = "Загружено";
+            this.button_color.UseVisualStyleBackColor = true;
+            this.button_color.Click += new System.EventHandler(this.button_color_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1388, 831);
+            this.Controls.Add(this.button_color);
+            this.Controls.Add(this.button_solve);
+            this.Controls.Add(this.textBox_info);
+            this.Controls.Add(this.button_ok);
+            this.Controls.Add(this.numericUpDownRow);
+            this.Controls.Add(this.numericUpDownClmn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownClmn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_solve;
+        private System.Windows.Forms.TextBox textBox_info;
+        private System.Windows.Forms.Button button_ok;
+        private System.Windows.Forms.NumericUpDown numericUpDownRow;
+        private System.Windows.Forms.NumericUpDown numericUpDownClmn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button button_color;
+    }
+}
+
---
+++ metodPotent/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace metodPotent
+{
+    public partial class Form1 : Form
+    {
+        int countClmn = 0;
+        int countRow = 0;
+        Dictionary<string, int> valueUV = new Dictionary<string, int>();
+        Dictionary<string, int> foundC = new Dictionary<string, int>();
+        Dictionary<string, int> foundDelta = new Dictionary<string, int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_ok_Click(object sender, EventArgs e)
+        {
+            valueUV.Clear();
+            foundC.Clear();
+            foundDelta.Clear();
+            textBox_info.Clear();
+            countClmn = (int)numericUpDownClmn.Value;
+            countRow = (int)numericUpDownRow.Value;
+            if (countClmn == 0 || countRow == 0) return;
+            while (dataGridView.Rows.Count > 0)
+            {
+                dataGridView.Rows.Clear();
+            }
+            while (dataGridView.Columns.Count > 0)
+            {
+                dataGridView.Columns.Clear();
+            }
+            for (int i = 0; i <= numericUpDownClmn.Value; i++)
+            {
+                if (i == 0)
+                {
+                    dataGridView.Columns.Add($"clmn{i}", "");
+                }
+                else
+                {
+                    dataGridView.Columns.Add($"clmn{i}", $"Заказчик {i}");
+                }
+            }
+            for (int i = 0; i <= numericUpDownRow.Value; i++)
+            {
+                dataGridView.Rows.Add();
+            }
+            dataGridView[0, 0].Value = "Запасы\\Требуется";
+        }
+
+        private void button_solve_Click(object sender, EventArgs e)
+        {
+            addToMap();
+            findC2();
+            findDelta();
+            needContinue();
+        }
+
+        private void button_color_Click(object sender, EventArgs e)
+        {
+            for(int i = 1; i <= countClmn; i++)
+            {
+                for(int j = 1; j <= countRow; j++)
+                {
+                    if(dataGridView[i, j].Selected)
+                    {
+                        dataGridView[i, j].Style.BackColor = Color.LightPink;
+
+                    }
+                }
+            }
+        }
+
+        private void needContinue()
+        {
+            foreach (var el in foundDelta)
+            {
+                if (el.Value < 0)
+                {
+                    textBox_info.Text += "\r\n  !!!НЕОБХОДИМО ПРОДОЛЖИТЬ ПОИСК!!!\r\n";
+                    return;
+                }
+            }
+            textBox_info.Text += "\r\n  !!!ПОЛУЧЕН ОПТИМАЛЬНЫЙ ПЛАН!!!\r\n";
+        }
+        private void findDelta()
+        {
+
+            textBox_info.Text += "\r\nРАСЧЕТ ОЦЕНОК СОБОДНЫХ КЛЕТОК\r\n";
+            for (int i = 1; i <= countRow; i++)
+            {
+                for (int j = 1; j <= countClmn; j++)
+                {
+                    if (dataGridView[j, i].Style.BackColor != Color.LightPink)
+                    {
+                        int c = Convert.ToInt32(dataGridView[j, i].Value);
+                        int c2 = foundC[$"C'[{i},{j}]"];
+                        int res = c - c2;
+                        foundDelta.Add($"Δ[{i},{j}]", res);
+                        textBox_info.Text += $"Δ[{i},{j}] = C[{i},{j}] - C'[{i},{j}] = {c} - {c2} = {res}\r\n";
+                    }
+                }
+            }
+        }
+        private void findC2()
+        {
+            textBox_info.Text += "\r\nРАСЧЕТ КОСВЕННЫХ ТАРИФОВ\r\n";
+            for (int i = 1; i <= countRow; i++)
+            {
+                for (int j = 1; j <= countClmn; j++)
+                {
+                    if (dataGridView[j, i].Style.BackColor != Color.LightPink)
+                    {
+                        int u = valueUV[$"U{i}"];
+                        int v = valueUV[$"V{j}"];
+                        int c = u + v;
+
+                        foundC.Add($"C'[{i},{j}]", c);
+                        textBox_info.Text += $"C'[{i},{j}] = U{i} + V{j} = {c}\r\n";
+                    }
+                }
+            }
+        }
+        private void addToMap()
+        {
+            textBox_info.Text += "\r\nРАСЧЕТ ПОТЕНЦИАЛОВ ЗАНЯТЫХ КЛЕТОК\r\n";
+            valueUV.Add("U1", 0);
+            for (int i = 1; i <= countRow; i++)
+            {
+                for (int j = 1; j <= countClmn; j++)
+                {
+                    if (dataGridView[j, i].Style.BackColor == Color.LightPink)
+                    {
+                        bool cntInU = false;
+                        bool cntInV = false;
+                        if (valueUV.ContainsKey($"U{i}"))
+                        {
+                            cntInU = true;
+                        }
+                        if (valueUV.ContainsKey($"V{j}"))
+                        {
+                            cntInV = true;
+                        }
+
+                        if(cntInU & !cntInV) // есть U, но нет V
+                        {
+                            var cellValue = Convert.ToInt32(dataGridView[j, i].Value);
+                            int v =  cellValue - valueUV[$"U{i}"];
+                            valueUV.Add($"V{j}", v);
+                            textBox_info.Text += $"U{i} = {valueUV[$"U{i}"]}    ";
+                            textBox_info.Text += $"U{i} + V{j} = {cellValue}  =>  V{j} = {v}\r\n";
+                        } else if(!cntInU & cntInV) // нет U, но есть V
+                        {
+                            var cellValue = Convert.ToInt32(dataGridView[j, i].Value);
+                            int u = cellValue - valueUV[$"V{j}"];
+                            valueUV.Add($"U{i}", u);
+                            textBox_info.Text += $"V{j} = {valueUV[$"V{j}"]}    ";
+                            textBox_info.Text += $"U{i} + V{j} = {cellValue}  =>  U{i} = {u}\r\n";
+                        } else if(cntInU & cntInV) // есть оба
+                        {
+                            continue;
+                        }else if(!cntInU & !cntInV) // оба отсутствуют
+                        {
+                            valueUV.Add($"U{i}", 0);
+                            --j;
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
